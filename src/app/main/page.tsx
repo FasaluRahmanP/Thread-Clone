c2d7cabbec4c400eb236862c56dbd9540d80970c
Fasalu Rahman <fadilrifna@gmail.com>
2024-09-24 11:48:25 +0530

    Thread-Clone

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -2,11 +2,29 @@ import React from 'react';
 
 const Page = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="h-full w-6/12 bg-[#201d1d] rounded-3xl">
-      
+    <>
+      <nav><h1 className='main-heading'>For you</h1></nav>
+      <div className="flex items-center justify-center h-screen">
+        <div className="h-full w-6/12 bg-[#201d1d] rounded-3xl">
+          <div className='main-posts-container'>
+            <div className='main-new-container'>
+              <div className='main-new'>
+                <div className='main-dp'>
+
+                </div>
+                <div className='main-text'>
+                  <span>What's new?</span>
+                </div>
+
+              </div>
+
+            </div>
+          </div>
+
+
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
