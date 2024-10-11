5356189a153c5df94901c0fecd4d2ffbae447904
Fasalu Rahman <fadilrifna@gmail.com>
2024-10-11 09:32:42 +0530

    Thread-Clone

--- src/app/main/Profile/page.tsx
+++ src/app/main/Profile/page.tsx
@@ -5,7 +5,7 @@ const Page = () => {
     <>
       <nav className='main-nav'><h1 className='main-heading'>Profile</h1></nav>
       <div className="flex items-center justify-center h-screen">
-        <div className="h-full w-6/12 bg-[#201d1d] rounded-3xl">
+        <div className="h-full w-6/12 bg-[#201d1d] rounded-3xl mt-11">
           
 
 
